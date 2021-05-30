642da5183803de04cb4a48c7310c8ccae13c8a9b
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-05-30 17:18:36 +0800

    jenkins test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">ERPGO 产品库存与销售系统 </h3>
+        <h3 class="title">jenkins Test</h3>
       </div>
 
       <el-form-item prop="username">
